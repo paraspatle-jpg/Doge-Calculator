68acd11ccaa5212b2363212ccbaf5538def3f5ed
paraspatle-jpg <paras18224827@gmail.com>
2021-11-04 23:31:10 +0530

    updated the functionality

--- script.js
+++ script.js
@@ -3,20 +3,74 @@ class Calculator {
     this.previousOperandandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
     this.clear();
+    this.updateDisplay();
   }
 
   clear() {
-    this.currentOperand = "";
+    this.currentOperand = "0";
     this.previousOperand = "";
-    this.operation = undefined;
+    this.operationString = "";
   }
   addNumber(number) {
+    if (number.toString() === "." && this.currentOperand.includes(".")) {
+      return;
+    }
     this.currentOperand = this.currentOperand + number.toString();
   }
-  delete() {
-    this.currentOperand = this.currentOperand.slice(0, -1);
+
+  operator(action) {
+    this.currentOperand = this.currentOperand + " ";
+    this.operationString = this.operationString + action.toString();
   }
+
+  delete() {
+    if (this.currentOperand[this.currentOperand.length - 1] === " ") {
+      this.operationString = this.operationString.slice(0, -1);
+    }
+
+    this.currentOperand = this.currentOperand.slice(0, -1);
+
+    if (this.currentOperand === "") {
+      this.currentOperand = "0";
+    }
+  }
+
+  /*calculateResult() {
+    let arr;
+    const str = "";
+    const temp = this.currentOperand;
+    temp.forEach((element) => {
+      if (element === " ") {
+        arr.push(Number(str));
+        str = "";
+      }
+      str = str + element;
+    });
+    const answer = arr[0];
+    for (let i = 0; i < this.operationString.length; i++) {
+      if (this.operationString[i] === "+") {
+        answer = answer + arr[i + 1];
+      }
+      if (this.operationString[i] === "-") {
+        answer = answer - arr[i + 1];
+      }
+      if (this.operationString[i] === "*") {
+        answer = answer * arr[i + 1];
+      }
+      if (this.operationString[i] === "/") {
+        answer = answer / arr[i + 1];
+      }
+      if (this.operationString[i] === "%") {
+        answer = answer % arr[i + 1];
+      }
+    }
+    this.currentOperand = arr.toString();
+  }*/
+
   updateDisplay() {
+    if (this.currentOperand[0] === "0" && this.currentOperand.length > 1) {
+      this.currentOperand = this.currentOperand.substring(1);
+    }
     this.currentOperandTextElement.innerText = this.currentOperand;
   }
 }
@@ -49,6 +103,14 @@ allClearButton.addEventListener("click", () => {
   cal.clear();
   cal.updateDisplay();
 });
-operationButtons.addEventListener("click", () => {
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    cal.operator(button.innerText);
+    cal.updateDisplay();
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  cal.calculateResult();
   cal.updateDisplay();
 });
